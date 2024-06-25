41183e6d29d4ea30f851010da0af0eff06c3a6cd
Ameen Alam~ <ameenalam98@gmail.com>
2024-06-25 19:41:49 +0500

    created place order feature and stripe payment integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
-
 const App = () => {
 
   const [showLogin,setShowLogin] = useState(false)
@@ -17,9 +16,9 @@ const App = () => {
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/order" element={<PlaceOrder/>} />
         </Routes>
       </div>
       <Footer />
