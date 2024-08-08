79af0d616f0d29877e05f0f4b44bbc7ad7d09046
Ameen Alam~ <ameenalam98@gmail.com>
2024-08-08 16:26:19 +0500

    added verify order route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import MyOrders from "./pages/MyOrders/MyOrders";
+import Verify from "./pages/Verify/Verify";
 const App = () => {
 
   const [showLogin,setShowLogin] = useState(false)
@@ -20,6 +21,7 @@ const App = () => {
           <Route path='/' element={<Home/>} />
           <Route path='/cart' element={<Cart/>} />
           <Route path='/order' element={<PlaceOrder/>} />
+          <Route path="/verify" element={<Verify />} />
           <Route path='/myorders' element={<MyOrders/>} />
         </Routes>
       </div>
